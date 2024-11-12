d6dcd53693b65a9bb1e26514b4fdcabc513e5e4c
Hristiyan Nikolov <hristiyan.at.nikoloff@gmail.com>
2024-11-12 22:21:32 +0200

    Fix styles in footer and homepage, display Bulgarian text on photo categories enum

--- OwlStock.Domain/Enumerations/Category.cs
+++ OwlStock.Domain/Enumerations/Category.cs
@@ -1,27 +1,70 @@
-﻿namespace OwlStock.Domain.Enumerations
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OwlStock.Domain.Enumerations
 {
     public enum Category
     {
+        [Display(Name = "Пейзаж")]
         Landscape = 1,
+
+        [Display(Name = "Природа")]
         Nature = 2,
+
+        [Display(Name = "Стрийт")]
         Street = 3,
+
+        [Display(Name = "Шарки")]
         Patterns = 4,
+
+        [Display(Name = "Животни")]
         Animals = 5,
+
+        [Display(Name = "Мода")]
         Fashion = 6,
+
+        [Display(Name = "Храна")]
         Food = 7,
+
+        [Display(Name = "Тапети")]
         Wallpapers = 8,
+
+        [Display(Name = "Пътешествия")]
         Travel = 9,
+
+        [Display(Name = "Хора")]
         People = 10,
+
+        [Display(Name = "Портрети")]
         Portrait = 11,
+
+        [Display(Name = "Бизнес")]
         Business = 12,
+
+        [Display(Name = "Спорт")]
         Sport = 13,
+
+        [Display(Name = "Автомобилна")]
         Automotive = 14,
+
+        [Display(Name = "Здраве")]
         Health = 15,
+
+        [Display(Name = "Изкуство")]
         Arts = 16,
+
+        [Display(Name = "Архитектура")]
         Architectural = 17,
+
+        [Display(Name = "Събития")]
         Events = 18,
+
+        [Display(Name = "Нощна")]
         Night = 19,
+
+        [Display(Name = "Астро")]
         Astro = 20,
+
+        [Display(Name = "Макро")]
         Macro = 22
     }
 }
