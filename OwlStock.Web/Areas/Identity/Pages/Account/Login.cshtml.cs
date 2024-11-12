90b95ad826cb7d58df13015963ad7ea22b5e6ebf
Hristiyan Nikolov <hristiyan.at.nikoloff@gmail.com>
2024-11-12 21:44:22 +0200

    Translate email validation messages

--- OwlStock.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ OwlStock.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -59,8 +59,8 @@ namespace OwlStock.Web.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [EmailAddress]
+            [Required(ErrorMessage = "Имейл е задължително поле")]
+            [EmailAddress(ErrorMessage = "Въведете валиден имейл адрес")]
             [Display(Name = "Имейл")]
             public string Email { get; set; }
 
