3314db6fd73f419593371d43909aac6e1ce5ab85
Hristiyan Nikolov <hristiyan.at.nikoloff@gmail.com>
2025-01-03 14:51:25 +0200

    add sitemap

--- OwlStock.Web/Controllers/StaticContentController.cs
+++ OwlStock.Web/Controllers/StaticContentController.cs
@@ -4,24 +4,60 @@ namespace OwlStock.Web.Controllers
 {
     public class StaticContentController : Controller
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public StaticContentController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
         public IActionResult Contacts()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Services()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Pricing()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult About()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ContentResult Sitemap()
+        {
+            string sitemapPath = Path.Combine(_webHostEnvironment.WebRootPath, "sitemap.xml");
+
+            if (!System.IO.File.Exists(sitemapPath))
+            {
+                return new ContentResult
+                {
+                    Content = "",
+                    ContentType = "application/xml",
+                    StatusCode = 404
+                };
+            }
+
+            string sitemap = System.IO.File.ReadAllText(sitemapPath);
+
+            return new ContentResult
+            {
+                Content = sitemap,
+                ContentType = "application/xml",
+                StatusCode = 200
+            };
         }
     }
 }
