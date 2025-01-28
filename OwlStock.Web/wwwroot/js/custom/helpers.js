c1523e8ce4f2fe3192245c17585ea7f32a9f17a6
Hristiyan Nikolov <hristiyan.at.nikoloff@gmail.com>
2025-01-28 22:57:36 +0200

    filter select list for photoshoot types on home page

---
+++ OwlStock.Web/wwwroot/js/custom/helpers.js
@@ -0,0 +1,11 @@
+﻿function filterPhotoshootTypeOptions() {
+    $(".list .option").each(function () {
+        // Get the text of the current option
+        let optionText = $(this).text();
+
+        // Check if it contains "Plus" or "Extra"
+        if (optionText.includes("Plus") || optionText.includes("Extra")) {
+            $(this).css("display", "none"); // Hide the option
+        }
+    });
+}
