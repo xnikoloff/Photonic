c9cb5f0e8124a6460e6a42aadb113fc73b8a1c73
Hristiyan Nikolov <hristiyan.at.nikoloff@gmail.com>
2025-02-13 01:10:25 +0200

    show/hide password

---
+++ OwlStock.Web/wwwroot/js/custom/passwordVisibility.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $(".btn-password-show").click(function () {
+        $(".btn-password-show").css("display", "none");
+        $(".btn-password-hide").css("display", "inline");
+        $("#Input_Password").attr("type", "text");
+    });
+
+    $(".btn-password-hide").click(function () {
+        $(".btn-password-show").css("display", "inline");
+        $(".btn-password-hide").css("display", "none");
+        $("#Input_Password").attr("type", "password");
+    });
+});
