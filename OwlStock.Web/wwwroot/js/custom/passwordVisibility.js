d75cc16a1bda0b38881da043ce8c7fc61b6e26c9
Hristiyan Nikolov <hristiyan.at.nikoloff@gmail.com>
2025-02-14 18:07:22 +0200

    redesign register and reset password forms

--- OwlStock.Web/wwwroot/js/custom/passwordVisibility.js
+++ OwlStock.Web/wwwroot/js/custom/passwordVisibility.js
@@ -3,11 +3,16 @@
         $(".btn-password-show").css("display", "none");
         $(".btn-password-hide").css("display", "inline");
         $("#Input_Password").attr("type", "text");
+        $("#Input_ConfirmPassword").attr("type", "text");
     });
 
     $(".btn-password-hide").click(function () {
         $(".btn-password-show").css("display", "inline");
         $(".btn-password-hide").css("display", "none");
         $("#Input_Password").attr("type", "password");
+        $("#Input_ConfirmPassword").attr("type", "password");
+
+        console.log($("#Input_ConfirmPassword").val());
+        console.log($("#Input_Password").val());
     });
 });
