fc2862331d562cf59e42408c4085321db7f1f5bf
Hristiyan Nikolov <hristiyan.at.nikoloff@gmail.com>
2025-04-19 03:00:23 +0300

    fix remember me is not clickable

--- OwlStock.Web/wwwroot/js/custom/helpers.js
+++ OwlStock.Web/wwwroot/js/custom/helpers.js
@@ -2,8 +2,7 @@
     $(".photoshoot-types-list option").each(function () {
         // Get the text of the current option
         let optionText = $(this).text();
-        console.log(optionText);
-
+        
         // Check if it contains "Plus" or "Extra"
         if (optionText.includes("Plus") || optionText.includes("Extra")) {
             $(this).remove(); // Hide the option
