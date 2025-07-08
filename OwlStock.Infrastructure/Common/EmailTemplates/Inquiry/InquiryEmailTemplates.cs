b1582b81550aadf4bfb8b24b972c9764c93bada6
Hristiyan Nikolov <hristiyan.at.nikoloff@gmail.com>
2025-07-08 23:49:20 +0300

    redesign inquiry email template

--- OwlStock.Infrastructure/Common/EmailTemplates/Inquiry/InquiryEmailTemplates.cs
+++ OwlStock.Infrastructure/Common/EmailTemplates/Inquiry/InquiryEmailTemplates.cs
@@ -4,7 +4,7 @@
     {
         public static string SendInquiryTemplate(string content, string name)
         {
-            return $"<!DOCTYPE html><html><body><div style=\"width:100%\"><h1 style=\"text-align:center\">Ново запитване</h1></div><div style=\"width:100%;;margin-top:50px;\"><h4 style=\"text-align:center;\">{content}</h4></div><div style=\"width:100%;margin-top:50px;\"><h4>{name}</h4></div></body></html>";
+            return $"<!doctypehtml><html lang=en><meta charset=UTF-8><meta content=\"width=device-width,initial-scale=1\"name=viewport><title>Запитване от {name}</title><body style=font-family:Verdana,Geneva,Tahoma,sans-serif><div style=width:100% id=container><div style=\"width:50%;margin:0 auto\"class=container-main><div style=margin-top:50px id=container-main-text><p style=font-size:1.2rem>{content}</div></div></div>";
         }
     }
 }
