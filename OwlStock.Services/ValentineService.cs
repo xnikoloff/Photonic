dd5cd6394f20881eeea63a212ff10cdd56d7c021
Hristiyan Nikolov <hristiyan.at.nikoloff@gmail.com>
2026-02-14 16:24:12 +0200

    Add valentine service

---
+++ OwlStock.Services/ValentineService.cs
@@ -0,0 +1,11 @@
+﻿namespace OwlStock.Services
+{
+    public class ValentineService
+    {
+        public async Task<bool> CreateRequest()
+        {
+            //TODO : Implement the logic to create a valentine request and save it to the database
+            throw new NotImplementedException();
+        }
+    }
+}
